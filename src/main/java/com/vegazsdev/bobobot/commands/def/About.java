e204d5c732accdab5f1b34786153aa9c004fe5d1
Hitalo <40531911+HitaloSama@users.noreply.github.com>
2020-08-25 23:35:49 -0300

    Revert "[ErfanGSIs-Hitsuki]testing some things"
    This reverts commit d429d850879eae36995d87d3feec9a6a34514794.

--- src/main/java/com/vegazsdev/bobobot/commands/def/About.java
+++ src/main/java/com/vegazsdev/bobobot/commands/def/About.java
@@ -24,7 +24,7 @@ public class About extends Command {
                         "[Google Drive API](https://developers.google.com/drive)\n" +
                         "[Google Guava](https://github.com/google/guava)\n" +
                         "[SQLite JDBC Driver (by xerial)](https://github.com/xerial/sqlite-jdbc)\n" +
-                        "[Hitsu³+ki](https://github.com/HitaloSama/Bot3)" +
+                        "[Rinne Berlinetta (Base: bo³+t Bot)](https://github.com/yukosky/Bot3)" +
                         "", update);
     }
 }
